ff8574c907bb913b1eb49fc7c3ab725a3919b9e0
Stephen Anderson and Nick Karpenske <dev+bendycode+randland@bendyworks.com>
2010-01-29 11:06:24 -0600

    Fixing deprecation warnings

--- theme_default_hamlized_extension.rb
+++ theme_default_hamlized_extension.rb
@@ -4,7 +4,9 @@ class ThemeDefaultHamlizedExtension < Spree::Extension
   url "http://github.com/bendyworks/spree-hamlized"
 
   def self.require_gems(config)
-    config.gem 'haml', :version => '2.2.8'
+    # HAML should be loaded in the extension, but it appears to load haml-edge
+    # TODO: Figure out why haml-edge is loaded, and remove haml declaration from spree root
+    # config.gem 'haml', :version => '2.2.8'
     config.gem 'compass', :version => '0.8.17'
   end
   
